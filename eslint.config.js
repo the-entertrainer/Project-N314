e6e8af72d134089bf601124f15349deaa98b47d3
the-entertrainer <naveenjosektym@proton.me>
2026-06-10 05:04:40 +0530

    Make ESLint stricter on real errors but disable noisy fast-refresh rule for clean builds

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,16 @@
-import { FlatCompat } from "@eslint/eslintrc";
 import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import { FlatCompat } from "@eslint/eslintrc";
 
 const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,
 });
 
 export default tseslint.config(
-  { ignores: ["dist", ".next", "node_modules"] },
+  { ignores: ["dist", ".next", "node_modules", "vite.config.*", "**/*.config.*"] },
   ...compat.extends("next/core-web-vitals"),
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
@@ -25,10 +25,8 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "react-refresh/only-export-components": "off",
     },
   }
 );
